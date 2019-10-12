ba7bf016fae6cb062606d45ddef7b35ff9360ea0
ImaneChafi <imane.chafi@mail.mcgill.ca>
2019-10-12 17:28:19 -0400

    Fixed some bugs

--- src/main/java/ca/mcgill/ecse321/projectGroup20/projectGroup20Application.java
+++ src/main/java/ca/mcgill/ecse321/projectGroup20/projectGroup20Application.java
@@ -1,19 +1,19 @@
-package ca.mcgill.ecse321.projectGroup20
+package ca.mcgill.ecse321.projectGroup20;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+//@RestController
 @SpringBootApplication
 public class projectGroup20Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(EventRegistrationApplication.class, args);
+    SpringApplication.run(projectGroup20Application.class, args);
   }
 
-  @RequestMapping("/")
+  //@RequestMapping("/")
   public String greeting(){
     return "Hello world, this is group 20's backend!";
   }
