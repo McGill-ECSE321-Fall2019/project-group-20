bd02b38501ebd38f8587ee9dc38e024417e8a532
ImaneChafi <imane.chafi@mail.mcgill.ca>
2019-10-13 12:03:20 -0400

    Added Ids and a build.gradle mainframe

--- src/main/java/ca/mcgill/ecse321/projectGroup20/model/SubjectMatter.java
+++ src/main/java/ca/mcgill/ecse321/projectGroup20/model/SubjectMatter.java
@@ -1,5 +1,13 @@
 package ca.mcgill.ecse321.projectGroup20.model;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 @Entity
+
 public abstract class SubjectMatter{
+	@Id	
+	private int id;
+	public int getId() {
+		return this.id;
+	}
 }
+
--- src/main/java/ca/mcgill/ecse321/projectGroup20/projectGroup20Application.java
+++ src/main/java/ca/mcgill/ecse321/projectGroup20/projectGroup20Application.java
@@ -2,10 +2,10 @@ package ca.mcgill.ecse321.projectGroup20;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-//@RestController
+@RestController
 @SpringBootApplication
 public class projectGroup20Application {
 
@@ -13,7 +13,7 @@ public class projectGroup20Application {
     SpringApplication.run(projectGroup20Application.class, args);
   }
 
-  //@RequestMapping("/")
+  @RequestMapping("/")
   public String greeting(){
     return "Hello world, this is group 20's backend!";
   }
