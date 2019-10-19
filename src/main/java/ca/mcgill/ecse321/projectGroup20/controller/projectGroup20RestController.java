a7efe1b10c6daa571e2fecf836c1c0864aa6dbba
ababs <adam.babs@mail.mcgill.ca>
2019-10-19 12:47:53 -0400

    Added the DTO package

---
+++ src/main/java/ca/mcgill/ecse321/projectGroup20/controller/projectGroup20RestController.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse321.projectGroup20.controller;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+public class projectGroup20RestController {
+
+}
---
+++ src/main/java/ca/mcgill/ecse321/projectGroup20/dto/RoomBookingdto.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.projectGroup20.dto;
+
+public class RoomBookingdto {
+
+}
