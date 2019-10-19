9726d8e102ae2083d95774e0ca1a19f745cc6a98
ababs <adam.babs@mail.mcgill.ca>
2019-10-19 13:30:31 -0400

    Controller and Tutorial Classes to test

--- src/main/java/ca/mcgill/ecse321/projectGroup20/controller/projectGroup20RestController.java
+++ src/main/java/ca/mcgill/ecse321/projectGroup20/controller/projectGroup20RestController.java
@@ -1,8 +1,20 @@
 package ca.mcgill.ecse321.projectGroup20.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @CrossOrigin(origins = "*")
 @RestController
 public class projectGroup20RestController {
+
+
+	
+
 
 }
