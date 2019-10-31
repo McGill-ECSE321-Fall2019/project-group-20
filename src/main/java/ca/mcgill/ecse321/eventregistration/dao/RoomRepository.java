001bb683aa7816fa7e7a78089498afef43841c50
ImaneChafi <imane.chafi@mail.mcgill.ca>
2019-10-31 10:15:42 -0400

    Update room

--- src/main/java/ca/mcgill/ecse321/eventregistration/dao/RoomRepository.java
+++ src/main/java/ca/mcgill/ecse321/eventregistration/dao/RoomRepository.java
@@ -1,5 +1,12 @@
 package ca.mcgill.ecse321.eventregistration.dao;
 
-public interface RoomRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.eventregistration.model.Room;
+
+public interface RoomRepository extends CrudRepository<Room, String> {
+
+	Room findRoomByNumber(int number);
 
 }
+
