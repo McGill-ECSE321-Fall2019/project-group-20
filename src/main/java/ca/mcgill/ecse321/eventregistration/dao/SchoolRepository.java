1834fd69824271a505f3620bea9e39312b372550
ImaneChafi <imane.chafi@mail.mcgill.ca>
2019-10-31 10:17:50 -0400

    Update SchoolRepository.java

--- src/main/java/ca/mcgill/ecse321/eventregistration/dao/SchoolRepository.java
+++ src/main/java/ca/mcgill/ecse321/eventregistration/dao/SchoolRepository.java
@@ -1,5 +1,11 @@
 package ca.mcgill.ecse321.eventregistration.dao;
 
-public interface SchoolRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.eventregistration.model.School;
+
+public interface SchoolRepository extends CrudRepository<School, String> {
+
+	School findSchoolByName(String name);
 
 }
