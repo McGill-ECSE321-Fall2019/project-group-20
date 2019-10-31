2851c73039c66360af79c54377994aadfcedf49f
ImaneChafi <imane.chafi@mail.mcgill.ca>
2019-10-31 19:05:06 -0400

    Added persistence tests for each class

--- src/test/java/ca/mcgill/ecse321/eventregistration/persistence/PersistenceTests.java
+++ src/test/java/ca/mcgill/ecse321/eventregistration/persistence/PersistenceTests.java
@@ -14,12 +14,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import ca.mcgill.ecse321.eventregistration.dao.BillRepository;
+import ca.mcgill.ecse321.eventregistration.dao.CompanyRepository;
 import ca.mcgill.ecse321.eventregistration.dao.CourseRepository;
 import ca.mcgill.ecse321.eventregistration.dao.EventRegistrationRepository;
+import ca.mcgill.ecse321.eventregistration.dao.FeedbackRepository;
 import ca.mcgill.ecse321.eventregistration.dao.PersonRepository;
+import ca.mcgill.ecse321.eventregistration.dao.RoomBookingRepository;
 import ca.mcgill.ecse321.eventregistration.dao.RoomRepository;
 import ca.mcgill.ecse321.eventregistration.dao.SchoolRepository;
+import ca.mcgill.ecse321.eventregistration.dao.SessionRepository;
+import ca.mcgill.ecse321.eventregistration.dao.SubjectRepository;
 import ca.mcgill.ecse321.eventregistration.dao.TutorRepository;
+import ca.mcgill.ecse321.eventregistration.dao.TutorReviewRepository;
 import ca.mcgill.ecse321.eventregistration.model.Event;
 import ca.mcgill.ecse321.eventregistration.service.EventRegistrationService;
 
@@ -51,6 +57,25 @@ public class PersistenceTests {
 	
 	@Autowired
 	private SchoolRepository schoolRepository;
+	
+	@Autowired
+	private CompanyRepository companyRepository;
+
+	@Autowired
+	private RoomBookingRepository roomBookingRepository;
+	
+	@Autowired
+	private FeedbackRepository feedbackRepository;
+	
+	@Autowired
+	private SessionRepository sessionRepository;
+	
+	@Autowired
+	private SubjectRepository subjectRepository;
+	
+	@Autowired
+	private TutorReviewRepository tutorReviewRepository;
+
 
 	@After
 	public void clearDatabase() {
@@ -60,6 +85,12 @@ public class PersistenceTests {
 		roomRepository.deleteAll();
 		billRepository.deleteAll();
 		schoolRepository.deleteAll();
+		companyRepository.deleteAll();
+		roomBookingRepository.deleteAll();
+		feedbackRepository.deleteAll();
+		sessionRepository.deleteAll();
+		subjectRepository.deleteAll();
+		tutorReviewRepository.deleteAll();
 	}
 
 	@Ignore
