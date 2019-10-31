cbc84ba0a0c109358ba6d044844cc535440ff4da
ImaneChafi <imane.chafi@mail.mcgill.ca>
2019-10-31 10:23:32 -0400

    Update RoomBookingRepository.java

--- src/main/java/ca/mcgill/ecse321/eventregistration/dao/RoomBookingRepository.java
+++ src/main/java/ca/mcgill/ecse321/eventregistration/dao/RoomBookingRepository.java
@@ -1,5 +1,11 @@
 package ca.mcgill.ecse321.eventregistration.dao;
 
-public interface RoomBookingRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.eventregistration.model.RoomBooking;
+
+public interface RoomBookingRepository extends CrudRepository<RoomBooking, String> {
+
+	RoomBooking findRoomBookingByRequestNb(String requestNb);
 
 }
