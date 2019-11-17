3e273d0a362c7b089ea9d192b4e7fda0e33e0b3a
mahroo12 <54996161+mahroo12@users.noreply.github.com>
2019-11-17 10:09:18 -0500

    final review form without session

--- ecse321G20-frontend/sofia_stuff/index.php
+++ ecse321G20-frontend/sofia_stuff/index.php
@@ -5,7 +5,10 @@
 </head>
 <body>
 	<form action="index.php" method="GET">
-		name: <input type="text" name="name">
+		Student's name: <input type="text" name="stname">
+		Tutor's name: <input type="text" name="tname">
+		Feedback: <input type="text" name="f">
+		<br>
 		<input type="submit" name="submit" value="add">
 	</form>
 
@@ -22,14 +25,17 @@ $con=mysqli_connect($host, $user, $pass, $db);
 
 if(isset($_GET['submit']))
 {
-	$rn = $_GET['name'];
+	$st = $_GET['stname'];
+	$t = $_GET['tname'];
+	$fd = $_GET['f'];
 	
 }
-$sql = "insert into try(s_no, name) values (null, '$rn')";
+
+$sql = "insert into review (`s`, `student name`, `tutor name`, `feedback`) values (null, '$st', '$t', '$fd')";
 
 $query =  mysqli_query($con, $sql);
 
-$m = "SELECT * FROM try ";
+$m = "SELECT * FROM review ";
 
 $records = mysqli_query($con, $m);
 echo "<hr>";
@@ -38,12 +44,19 @@ while($row = mysqli_fetch_array($records))
 	echo "<tr>";
 		//echo "entry number ";
 		//echo " ";
-		//echo "<td>". $row['s_no'].    "</td>";
+		//echo "<td>". $row['s'].    "</td>";
 		//echo "    ";
 		//echo " :  ";
 		echo "<br>";
 		echo "name of student:";
-		echo "<td>". $row['name'].    "</td>";
+		echo "<td>". $row['student name'].    "</td>";
+		echo "<br>";
+		echo "name of tutor:";
+		echo "<td>". $row['tutor name'].    "</td>";
+		echo "<br>";
+		echo "feedback:";
+		echo "<td>". $row['feedback'].    "</td>";
+		
 		echo "<br>";
 		echo "<br>";
 	echo "</tr>";
